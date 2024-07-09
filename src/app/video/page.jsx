34f9a1f550d78884fd3bdb5a2494a0da848f0fa3
Eng-abdelrhman-mohamed <157839745+Eng-abdelrhman-mohamed@users.noreply.github.com>
2024-07-09 18:18:50 +0300

    Add files via upload

---
+++ src/app/video/page.jsx
@@ -0,0 +1,10 @@
+
+export default function page() {
+  return (
+    <>
+    
+      <div style={{position: 'relative', paddingTop: '56.25%'}}><iframe src="https://iframe.mediadelivery.net/embed/267594/2e1108c3-1f56-4182-9c92-4416586e4175?autoplay=false&loop=false&muted=false&preload=false&responsive=true" loading="lazy" style={{border: 0, position: 'absolute', top: 0, height: '100%', width: '100%'}} allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;" allowFullScreen="true" /></div>
+    
+    </>
+  )
+}
